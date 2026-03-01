15d1d403104c2b849ea736bed38945dd1126a238
cccccccccccccccccnrd <hello@conradweise.com>
2026-03-01 22:47:48 +0100

    mh

--- design/front/ecosystem.config.cjs
+++ design/front/ecosystem.config.cjs
@@ -3,8 +3,6 @@ module.exports = {
     {
       name: 'equip-1',
       script: '.output/server/index.mjs',
-      instances: 'max',
-      exec_mode: 'cluster',
       env: {
         NODE_ENV: 'production',
         PORT: 3234
